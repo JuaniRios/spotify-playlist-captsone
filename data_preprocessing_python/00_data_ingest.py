c575cea93cb38714f95bef7009f2474447659e8e
sainioh <59020178+sainioh@users.noreply.github.com>
2022-03-09 20:03:09 +0100

    Optimized python data ingest

--- data_preprocessing_python/00_data_ingest.py
+++ data_preprocessing_python/00_data_ingest.py
@@ -1,41 +1,54 @@
-import pandas as pd
-import os
-import numpy as np
 import json
-from tqdm import tqdm
+import numpy as np
 from scipy.sparse import csr_matrix
 from sys import getsizeof
 from itertools import chain
+import time
 
-
+start = time.time()
+print("read data")
+# read data
 data = json.load(open('../../ML_AI/ds_capstone/data/mpd.slice.0-999.json'))
 
+print("parse")
+# parse json
 db = []
 for row in data["playlists"]:
-    #p_id = row["pid"]
+    # p_id = row["pid"]
     tracks = [song["artist_name"] + " - " + song["track_name"] for song in row["tracks"]]
     db.append(tracks)
 
+print("get unique songs")
 # get unique songs
-song_list = set(chain(*db))
+song_list = list(set(chain(*db)))
 
-mx = []
+song_map = {}
+for i, song in enumerate(song_list):
+    song_map[song] = i
 
-for row in tqdm(db):
-    temp = []
-    for s in song_list:
-        if s in row:
-            temp.append(1)
-        else:
-            temp.append(0)
+print("Turn into matrix")
 
-    mx.append(temp)
+# pre-fill matrix with zeroes
+
+mx = np.zeros((len(db), len(song_list)), dtype="int").tolist()
 
 
-sparseMatrix = csr_matrix(mx)
+for rowIdx, row in enumerate(db):
+    for dbSong in row:
+        mx[rowIdx][song_map[dbSong]] = 1
 
-# check sizes
+print("checksum db:", len(set(db[55])))
+print("checksum mx:", sum(mx[55]))
 
-print(getsizeof(mx))
+# to sparse matrix
+# sparseMatrix = csr_matrix(mx)
 
-print(getsizeof(sparseMatrix))
+# Compare usage of variables
+# print(getsizeof(mx))
+
+# print(getsizeof(sparseMatrix))
+
+# print("mx[0]", mx[0])
+
+end = time.time()
+print(f"Total time: {end-start}")
