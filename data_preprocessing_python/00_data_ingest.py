fd3a201c029e41858fe5d791548c83a28e4d8568
Daniel Netzl <netzl.daniel@gmail.com>
2022-03-31 11:42:28 +0200

    Added Hit Rate Evaluation

--- data_preprocessing_python/00_data_ingest.py
+++ data_preprocessing_python/00_data_ingest.py
@@ -8,20 +8,21 @@ import pickle
 
 
 #%% Environment Variables
-path = "../../ML_AI/ds_capstone/data/"  # Henrik
-# path = "C:/Users/netzl/Offline Documents/spotify_million_playlist_dataset/data/" # Daniel
+# path = "../../ML_AI/ds_capstone/data/"  # Henrik
+path = "C:/Users/netzl/Offline Documents/spotify_million_playlist_dataset/data/" # Daniel
 # path = "../data/spotify_million_playlist_dataset/data/" # Juan
-reduced_percentage = 3
-f_name_songlist = "allSongs_reduced_3.pickle" # name for song list pickle file
-f_name_mx = 'sparse_matrix_reduced_3.npz' # name for sparse matrix
-f_name_song_map = "song_map_3.pickle" # name for song_map pickle (to keep the order of songs in mx)
+reduced_percentage = 2
+f_name_songlist = "allSongs_reduced_2.pickle" # name for song list pickle file
+f_name_mx = 'sparse_matrix_reduced_2.npz' # name for sparse matrix
+f_name_song_map = "song_map_2.pickle" # name for song_map pickle (to keep the order of songs in mx)
 start = time.perf_counter()
-# os.chdir("./data_preprocessing_python")
+#os.chdir("/data_preprocessing_python")
 
 #%% Create list of playlists
 def pickle_playlists(filename, data_loc, percentage):
     print(os.getcwd())
     db = []
+    leftout_songs = []
     # get all files (dataset is divided)
     files = os.listdir(data_loc)
     amount_files = len(files)
@@ -32,11 +33,16 @@ def pickle_playlists(filename, data_loc, percentage):
         data = json.load(open(data_loc + file))
         # list of playlist are under the key "playlists"
         for row in data["playlists"]:
+            leftout_cutoff = int(round(len(row) * .9, 0))  # 90% of each playlist for creating recommendation
             tracks = [song["artist_name"] + " - " + song["track_name"] for song in row["tracks"]]
-            db.append(tracks)
+            db.append(tracks[1:leftout_cutoff])
+            leftout_songs.append(tracks[leftout_cutoff:]) # 10% for evaluation/calculating hit rate
 
     with open(filename, 'wb') as f:
         pickle.dump(db, f, pickle.HIGHEST_PROTOCOL)
+
+    with open("leftout.pickle", 'wb') as f:
+        pickle.dump(leftout_songs, f, pickle.HIGHEST_PROTOCOL)
 
 if not os.path.isfile(f_name_songlist):
     print("Pickle not found, creating it now. Please wait...")
@@ -56,14 +62,10 @@ print("Creating vectors for spare matrix")
 song_list = list(set(chain(*playlists)))
 song_map = {song:i for i,song in enumerate(song_list)}
 
-# create pickle file to preserve the order of songs in matrix columns
-with open(f_name_song_map, 'wb') as f:
-    pickle.dump(song_map, f, pickle.HIGHEST_PROTOCOL)
-
 data_m, row_ix, col_ix = [], [], []
 
 for rowIdx, rw in enumerate(playlists):
-    for dbSong in rw:
+    for dbSong in rw[1:]:
         row_ix.append(rowIdx)
         col_ix.append(song_map[dbSong])
 data_m = [1]*len(col_ix)
@@ -75,6 +77,10 @@ sparseMatrix = coo_matrix((data_m, (row_ix, col_ix)), shape=(len(playlists), len
 
 # Save file
 save_npz(f_name_mx, sparseMatrix)
+
+# Save Songmap
+with open(f_name_song_map, 'wb') as f:
+    pickle.dump(song_map, f, pickle.HIGHEST_PROTOCOL)
 
 # print time taken
 end = time.perf_counter()
--- models/20_simple_SVD_py.py
+++ models/20_simple_SVD_py.py
@@ -4,13 +4,16 @@ import pickle
 import numpy as np
 import pandas as pd
 
-
-# Load sparse matrix
+#%% Load sparse matrix
 mx = sparse.load_npz("../data_preprocessing_python/sparse_matrix_reduced_2.npz")
 
 # read playlists
 with open("../data_preprocessing_python/allSongs_reduced_2.pickle", 'rb') as f:
     playlists = pickle.load(f)
+
+# read leftout songs
+with open("../data_preprocessing_python/leftout.pickle", 'rb') as f:
+    leftout_songs = pickle.load(f)
 
 # Read song_map to recreate
 with open("../data_preprocessing_python/song_map_2.pickle", 'rb') as f:
@@ -20,11 +23,16 @@ with open("../data_preprocessing_python/song_map_2.pickle", 'rb') as f:
 inv_map = {v: k for k, v in smap.items()}
 
 
-# SVD
+#%% SVD
 
 # Decompose to matrices from sparse matrix. "svds" is a sparse option for standard "svd".
 # Still have to figure out how number of eigenvalues (k) matters to predictions
-U, sigma, V = svds(mx, k = 15)
+
+# To avoid ValueError: matrix type must be 'f', 'd', 'F', or 'D'
+# upcast matrix to fload or double.
+mx = mx.asfptype()
+# create matrices
+U, sigma, V = svds(mx, k=15)
 
 # diagonal matrix of eigenvalues, needed for reconstruction
 sigma = np.diag(sigma)
@@ -32,19 +40,30 @@ sigma = np.diag(sigma)
 # reconstruct original matrix by producing dot product U . Sigma . V
 recommendations = np.dot(np.dot(U, sigma), V)
 
-
+#%% Reconstruct Matrix for Recommendations
 # Reconstructed matrix in dataframe format. Column names set to song names
 # !!!! The values are not probabilities yet. Need to normalize the values to scale 0-1
-preds_df = pd.DataFrame(recommendations, columns = list(smap.keys()))
+preds_df = pd.DataFrame(recommendations, columns=list(smap.keys()))
 preds_df.head()
 
+#%% Access Recommendations by Playlist Index
+# Show top 10 recommendations. Index means a specific playlist you want to see the recommendations for
+# track hitrate
+hr = []
+for i in range(len(playlists)):
+    idx = i  # index for specific playlist
+    print(i/len(playlists))
+    # Top 20 recommendations, songs already present in playlist are removed
+    #print("Top 20 Recommendations")
+    SVD_recs = [s for s in preds_df.iloc[idx].sort_values(ascending=False).index if s not in playlists[idx]][:20]
+    #print(SVD_recs, end="\n\n\n")
 
-# Show top 5 recommendations. Index means a specific playlist you want to see the recommendations for
-idx = 1091 # index for specific playlist
+    # Calculate Hit Rate
+    hr.append(sum([r in leftout_songs[idx] for r in SVD_recs]))
+    #print("Hit Rate:")
+    #print(hr[i])
 
-# Top 5 recommendations, songs already present in playlist are removed
-print("Top 5 Recommendations")
-print([s for s in preds_df.iloc[idx].sort_values(ascending=False).index if s not in playlists[idx]][:5], end="\n\n\n")
-
-# Show 20 first songs from playlist to compare the recommendations. Use same index as previous cell
-print(playlists[idx][:20])
+    # Show 20 first songs from playlist to compare the recommendations. Use same index as previous cell
+    #print(playlists[idx][:20])
+print("Mean Hit Rate:")
+print(sum(hr)/len(hr))
