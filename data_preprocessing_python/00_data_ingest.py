66bed7f6e33fa5a69498db81dff442f75d3afade
Daniel Netzl <netzl.daniel@gmail.com>
2022-04-04 14:39:02 +0200

    small refactoring for readability

--- data_preprocessing_python/00_data_ingest.py
+++ data_preprocessing_python/00_data_ingest.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     reduced_percentage = 2
     file_all_songs = "allSongs.pickle"  # name for song list pickle file
 
-    file_MF_songs = f"songlist_for_MF_reduced_{reduced_percentage}.pickle"  # input songs for MF
+    file_MF_songs = f"songlist_for_MF_reduced_{reduced_percentage}.pickle"  # input songs for Matrix factorization
     file_leftout_songs = f"leftout_{reduced_percentage}.pickle"  # name for list of left out songs
 
-    file_mx_reduced = f'sparse_matrix_reduced_{reduced_percentage}.pickle'  # name for MF input sparse matrix
     file_songmap_reduced = f'song_map_{reduced_percentage}.pickle' # reduced song_map
+    file_mx_reduced = f'sparse_matrix_reduced_{reduced_percentage}.pickle'  # name for MF input sparse matrix
 
     file_songmap = "song_map.pickle"  # full song_map
     file_mx_full = "sparseMatrix_full.npz"  # name for full matrix (used in EDA)
@@ -25,6 +25,7 @@ if __name__ == "__main__":
 
     start = time.perf_counter()
 
+    # getting all songs (load or create)
     if not (os.path.isfile(file_all_songs)):
         print("Creating full playlist.")
         all_songs = dump_all_songs(file_all_songs, path)
@@ -33,6 +34,7 @@ if __name__ == "__main__":
         with open(file_all_songs, 'rb') as f:
             all_songs = pickle.load(f)
 
+    # getting leftout and matrix factorization songs (load or create)
     if not (os.path.isfile(file_MF_songs) & os.path.isfile(file_leftout_songs)):
         print("Creating reduced playlists.")
         MF_songs, leftout_songs = create_playlists(file_MF_songs, file_leftout_songs, path, reduced_percentage = 2)
@@ -43,6 +45,7 @@ if __name__ == "__main__":
         with open(file_leftout_songs, 'rb') as f:
             leftout_songs = pickle.load(f)
 
+    # getting songmap (name => index)
     if not (os.path.isfile(file_songmap)):
         print("Creating full song map.")
         song_map = create_song_map(file_songmap, all_songs, percentage = 100)
@@ -51,6 +54,7 @@ if __name__ == "__main__":
         with open(file_songmap, 'rb') as f:
             song_map = pickle.load(f)
 
+    # getting the reduced version of songmap
     if not (os.path.isfile(file_songmap_reduced)):
         print("Creating reduced song map.")
         song_map_reduced = create_song_map(file_songmap_reduced, all_songs, percentage = reduced_percentage)
@@ -59,6 +63,7 @@ if __name__ == "__main__":
         with open(file_songmap_reduced, 'rb') as f:
             song_map_reduced = pickle.load(f)
 
+    # getting the full sparse matrix
     if not (os.path.isfile(file_mx_full)):
         print("Creating full sparse matrix.")
         sparseMatrix_full = create_sparse_matrix(file_mx_full, all_songs, song_map)
@@ -67,6 +72,7 @@ if __name__ == "__main__":
         with open(file_mx_full, 'rb') as f:
             sparseMatrix_full = load_npz(f)
 
+    # getting the reduced sparse matrix
     if not (os.path.isfile(file_mx_reduced)):
         print("Creating reduced sparse matrix.")
         sparseMatrix_reduced = create_sparse_matrix(file_mx_reduced, MF_songs, song_map_reduced)
--- data_preprocessing_python/02_EDA.py
+++ data_preprocessing_python/02_EDA.py
@@ -13,89 +13,87 @@ file_songmap = "song_map.pickle"  # full song_map
 file_mx_full = "sparseMatrix_full.npz"  # name for full matrix (used in EDA)
 file_all_songs = "allSongs.pickle" #all playlists
 
-if not (os.path.isfile(filename)):
-    print("Loading files...")
 
-    try:
-        mx = sparse.load_npz(file_mx_full)
+if (os.path.isfile(filename)):
+    raise Exception("EDA file already exists.")
 
-        # Read song_map to recreate
-        with open(file_songmap, 'rb') as f:
-            songmap = pickle.load(f)
+print("Loading files...")
 
-        with open(file_all_songs, 'rb') as f:
-            playlists = pickle.load(f)
+try:
+    mx = sparse.load_npz(file_mx_full)
 
-        success = True
-    except:
-        print("Some file(s) not found.")
-        success = False
+    # Read song_map to recreate
+    with open(file_songmap, 'rb') as f:
+        songmap = pickle.load(f)
 
-    if success:
-        start = time.perf_counter()
+    with open(file_all_songs, 'rb') as f:
+        playlists = pickle.load(f)
 
-        # inverse song_map to easily access songs by index
-        inv_map = {v: k for k, v in songmap.items()}
+except:
+    raise Exception("Some file(s) not found.")
 
-        #%% Popularity
-        print("Doing calculations...")
-        # calculate column sums; column = song
-        colSums = np.asarray(mx.sum(axis=0)) # axis 0 -> column sum; axis 1 -> rows sum
-        # save column sum. ind = (row index, column index); cSum = column sum.
-        # access song by column index (ind[1]) from inv_map.
-        # descending order by column sum
-        # output: list of tuples
-        popularity_dict = sorted({inv_map[ind[1]]:cSum for ind,cSum in np.ndenumerate(colSums)}.items(),
-                                 key=lambda x:x[1], reverse = True)
-        print(f"Top 10 most popular songs: {popularity_dict[:10]}")
+start = time.perf_counter()
 
-        # get total number of songs
-        # sum up all columns sums
-        songs_sum = sum([n for song,n in popularity_dict])
-        print(f"Total number of songs (not unique): {songs_sum}")
+# inverse song_map to easily access songs by index
+inv_map = {v: k for k, v in songmap.items()}
 
-        # get songs which only appear once
-        # all songs where column sum is 1
-        unique_songs = [song for song,n in popularity_dict if n == 1]
-        print(f"Total number of songs which only appear once: {len(unique_songs)}, i.e. {round(len(unique_songs)/songs_sum*100,1)}%")
+#%% Popularity
+print("Doing calculations...")
+# calculate column sums; column = song
+colSums = np.asarray(mx.sum(axis=0)) # axis 0 -> column sum; axis 1 -> rows sum
+# save column sum. ind = (row index, column index); cSum = column sum.
+# access song by column index (ind[1]) from inv_map.
+# descending order by column sum
+# output: list of tuples
+popularity_dict = sorted({inv_map[ind[1]]:cSum for ind,cSum in np.ndenumerate(colSums)}.items(),
+                         key=lambda x:x[1], reverse = True)
+print(f"Top 10 most popular songs: {popularity_dict[:10]}")
+
+# get total number of songs
+# sum up all columns sums
+songs_sum = sum([n for song,n in popularity_dict])
+print(f"Total number of songs (not unique): {songs_sum}")
+
+# get songs which only appear once
+# all songs where column sum is 1
+unique_songs = [song for song,n in popularity_dict if n == 1]
+print(f"Total number of songs which only appear once: {len(unique_songs)}, i.e. {round(len(unique_songs)/songs_sum*100,1)}%")
 
 
-        # avg playlist length
-        mean_playlist_len = sum(len(pl) for pl in playlists) / len(playlists)
-        print(f"Avg. number of songs in a playlist: {mean_playlist_len}")
+# avg playlist length
+mean_playlist_len = sum(len(pl) for pl in playlists) / len(playlists)
+print(f"Avg. number of songs in a playlist: {mean_playlist_len}")
 
-        # identical playlists
-        # Sort the playlists first.
-        identical_playlists = 0
-        n = 0 # track progress
-        total = len(playlists)
-        for pl in playlists:
-            pl.sort() # sort playlists, otherwise there won't be any match
-            n+=1
-            print(f"{round(n / total * 100,2)}% done.")
+# identical playlists
+# Sort the playlists first.
+identical_playlists = 0
+n = 0 # track progress
+total = len(playlists)
+for pl in playlists:
+    pl.sort() # sort playlists, otherwise there won't be any match
+    n+=1
+    print(f"{round(n / total * 100,2)}% done.")
 
-        # convert to np array for faster processing
-        playlists = np.array(playlists, dtype=object)
-        # store each playlist with its counts
-        _, counts = np.unique(playlists, return_counts=True)
-        # sum up playlists which appear more than once
-        print(f"Total number of duplicate playlists: {sum(counts>1)}")
+# convert to np array for faster processing
+playlists = np.array(playlists, dtype=object)
+# store each playlist with its counts
+_, counts = np.unique(playlists, return_counts=True)
+# sum up playlists which appear more than once
+print(f"Total number of duplicate playlists: {sum(counts>1)}")
 
-        # print time taken
-        end = time.perf_counter()
-        print(f"\n\nTOTAL RUN DURATION: {end-start}")
+# print time taken
+end = time.perf_counter()
+print(f"\n\nTOTAL RUN DURATION: {end-start}")
 
-        #%% Export results
-        results = {"Top 10 most popular songs": f"{popularity_dict[:10]}",
-                   "Total number of songs (not unique)": f"{songs_sum}",
-                   "Total number of songs which only appear once": f"{len(unique_songs)}",
-                   "Percentage of unique songs": f"{round(len(unique_songs)/songs_sum*100,1)}",
-                   "Avg. number of songs in a playlist": f"{mean_playlist_len}",
-                   "Total number of duplicate playlists": f"{sum(counts>1)}"
-                   }
+#%% Export results
+results = {"Top 10 most popular songs": f"{popularity_dict[:10]}",
+           "Total number of songs (not unique)": f"{songs_sum}",
+           "Total number of songs which only appear once": f"{len(unique_songs)}",
+           "Percentage of unique songs": f"{round(len(unique_songs)/songs_sum*100,1)}",
+           "Avg. number of songs in a playlist": f"{mean_playlist_len}",
+           "Total number of duplicate playlists": f"{sum(counts>1)}"
+           }
 
-        # dump results to json file
-        with open(filename, "w") as outfile:
-            json.dump(results, outfile, indent=4)
-else:
-    print("EDA file already exists.")
+# dump results to json file
+with open(filename, "w") as outfile:
+    json.dump(results, outfile, indent=4)
